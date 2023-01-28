ee3be53e6ad313e6b5a9b5e6880ef0252513fd2e
PONzu-0529 <yatponzu@gmail.com>
2023-01-28 22:49:09 +0900

    Add Vue Routing

--- httpd/setup.sh
+++ httpd/setup.sh
@@ -7,7 +7,11 @@ sudo a2enmod rewrite
 sudo chmod 777 -R /home/${XSERVER_ID}/${XSERVER_DOMAIN}/public_html
 
 # Setup Link
-ln -s /home/${XSERVER_ID}/${XSERVER_DOMAIN}/repos/public /home/${XSERVER_ID}/${XSERVER_DOMAIN}/public_html/${XSERVER_SUBDOMAIN}
+ln -s /home/${XSERVER_ID}/${XSERVER_DOMAIN}/repos/dist /home/${XSERVER_ID}/${XSERVER_DOMAIN}/public_html/${XSERVER_SUBDOMAIN}
+
+# Change Permittion
+sudo chmod -R 777 storage/*
+sudo chmod -R 777 bootstrap/cache
 
 # Change Owner
 sudo chown -R 1000 /home/${XSERVER_ID}/${XSERVER_DOMAIN}/repos/vendor
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/laravel', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return makeVueResponse();
+});
+
+Route::get('/{any}', function () {
+    return makeVueResponse();
+});
+
+function makeVueResponse()
+{
+    return response()
+        ->file('index.html', ['Content-Type' => 'text/html']);
+}
